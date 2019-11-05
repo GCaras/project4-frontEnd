00ef5cb43d2534d9f40be63869901eef26618563
GCaras <gcaras0@gmail.com>
2019-11-04 21:21:02 -0500

    cleaned up splashpage with unused variables

--- src/components/Splashpage.js
+++ src/components/Splashpage.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import styled from 'styled-components'
 import BlogPostList from './BlogPostList'
 
@@ -8,15 +7,6 @@ const StyledButtonContainer = styled.section`
     display: flex;
     flex-direction: column;
     justify-content: center;
-`
-
-const StyledClickH2 = styled.h2`
-    background-color: lightskyblue;
-    border: 2px solid black;
-    margin: 5px;
-    padding: 5px;
-    max-width: 125px;
-    min-width: 110px;
 `
 
 const SplashPage = () => {
